24cad992fa6b3d8e6d811534c60df60553222649
andreassatro-ops <andreassatro@gmail.com>
2026-02-03 19:37:22 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+let start = 0
+let score = 0
+let elapsed = 0
+input.onButtonPressed(Button.A, function () {
+    start = 7 - 1
+})
+input.onButtonPressed(Button.B, function () {
+    start = 6 - 1
+    score += 1
+    elapsed = 0
+    basic.showLeds(`
+        # . . . #
+        . # # # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # . # #
+        # . # . #
+        # # . # #
+        # # # # #
+        `)
+})
